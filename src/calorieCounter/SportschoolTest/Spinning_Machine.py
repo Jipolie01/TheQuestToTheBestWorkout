cf08c996dc79e74b0744ed5b9403400375c90104
Jip <jipgalema@hotmail.com>
2016-01-13 19:36:49 +0100

    Spinning machine added

---
+++ src/calorieCounter/SportschoolTest/Spinning_Machine.py
@@ -0,0 +1,15 @@
+# This file is used for the spinning machine in the gym
+from tkinter import *
+import time
+
+
+def main():
+    """
+    This is the main function, all the function will be called from here
+    """
+
+
+def spinningMachine():
+    """
+    This function is used to count the amount of calories the client uses while spinning
+    """
