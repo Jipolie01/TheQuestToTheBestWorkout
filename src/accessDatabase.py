c86e5338ac7afaa01cd143cb916b9aa58d191bc7
Maarten <maarten.deklerk@gmail.com>
2016-01-13 14:22:07 +0100

    added docstring to accesDatabase.py

--- src/accessDatabase.py
+++ src/accessDatabase.py
@@ -1,16 +1,20 @@
 import sqlite3
-
+"""opens the client database and sets a cursor in the file"""
 conn = sqlite3.connect('sql/clientDatabase.db')
 c = conn.cursor()
 
+
+"""makes and empty list"""
 rows = []
 
+"""Selects a column from the client database and removes brackets and quotation marks."""
 def readData(column):
     for row in c.execute("SELECT " +column+ " FROM clientInfo"):
         rowN = (str(row).replace(')', '').replace('(','').replace("'", ""))
         rowN = rowN[:-1]
         rows.append(rowN)
 
+"""Checks if an item is in de rows list and returns true or false based on that."""
 def checkAccess(column, item):
     readData(column)
     if item in rows:
