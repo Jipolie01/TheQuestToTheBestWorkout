57e65073392e59f1b603a612811c895ac59e79aa
Jip <jipgalema@hotmail.com>
2016-01-14 13:25:34 +0100

    Weightlifting file completed

--- src/calorieCounter/SportschoolTest/Weightlifting.py
+++ src/calorieCounter/SportschoolTest/Weightlifting.py
@@ -10,6 +10,8 @@ def main():
     """
     This function is used to call all other function in this file
     """
+    interface()
+    weightlifting()
 
 
 def interface():
@@ -17,12 +19,9 @@ def interface():
     This function is used to call the interface for the weightlifting
     """
     window = Tk()
-
     Button(window, text="START", command=startTime).grid(row=0)
     Button(window, text="STOP", command=stopTime).grid(row=0, column=1)
-    Label(window)
-
-
+    Button(window, text="Bereken calorieën", command=window.destroy).grid(row=1)
     window.mainloop()
 
 
@@ -30,15 +29,37 @@ def weightlifting():
     """
     This function is used to count te calories used by the client
     """
-
+    liftingWeight = 75
+    clientWeight = 70
+    startWeight = 10
+    global sportTime
+    print(sportTime)
+    if liftingWeight <= 80:
+        startCalorie = 32
+        calorieTotal = (startCalorie * ((clientWeight/startWeight)-1)) + startCalorie
+        actualCalorie = calorieTotal * sportTime
+        print(int(actualCalorie), "Kcal")
+    elif liftingWeight > 80:
+        startCalorie = 63
+        calorieTotal = (startCalorie * ((clientWeight/startWeight)-1)) + startCalorie
+        actualCalorie = calorieTotal * sportTime
+        print(int(actualCalorie), "Kcal")
 
 
 def startTime():
     """
     This function is used to start the timer, this function is called when the start button is pressed
     """
+    global start
+    start = time.clock()
+
 
 def stopTime():
     """
     This function is used to stop the timer, and is called when the stop button is pressed
     """
+    global sportTime
+    sportTime = (time.clock() - start)/3600
+
+
+main()
