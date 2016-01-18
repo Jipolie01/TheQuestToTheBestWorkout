b5de6a4773b63caf2553874f3e4cba703a03469c
Maarten <maarten.deklerk@gmail.com>
2016-01-18 11:46:41 +0100

    made mysql database

--- src/sql/clientDatabase.sql
+++ src/sql/clientDatabase.sql
@@ -1,46 +1,11 @@
-BEGIN TRANSACTION;
-CREATE TABLE "subscription" (
-	`subscriptionName`	TEXT,
-	`price`	INT,
-	PRIMARY KEY(subscriptionName),
-	FOREIGN KEY(`subscriptionName`) REFERENCES clienInfo ( subscriptionName )
+CREATE TABLE IF NOT EXISTS costumerInfo (
+	costumerID		 INT UNSIGNED		NOT NULL AUTO_INCREMENT,
+    surname			 VARCHAR(30)		NOT NULL DEFAULT '',
+    name 			 VARCHAR(30)		NOT NULL DEFAULT '',
+    gender			 VARCHAR(6)			NOT NULL DEFAULT '',
+    RFIDNumber		 CHAR(20)			NOT NULL DEFAULT '[00,000,000,000,000]',
+    cellphoneNumber  CHAR(11)			NOT NULL DEFAULT '06-00000000',
+    subscriptionName VARCHAR(5)			NOT NULL DEFAULT '',
+    
+    primary key(costumerID)
 );
-INSERT INTO `subscription` VALUES ('easy','17,99');
-INSERT INTO `subscription` VALUES ('smart','19,99');
-INSERT INTO `subscription` VALUES ('flex','23,99');
-CREATE TABLE "logInfo" (
-	`rfidNumber`	TEXT,
-	`username`	TEXT,
-	`password`	TEXT,
-	`email`	TEXT,
-	PRIMARY KEY(rfidNumber),
-	FOREIGN KEY(`rfidNumber`) REFERENCES clientInfo ( rfidNumber )
-);
-INSERT INTO `logInfo` VALUES ('[12,235,179,213,129]','user2','user2','user2@hotmail.com');
-INSERT INTO `logInfo` VALUES ('[52,188,189,222,235]','user1','user1','user1@hotmail.com');
-CREATE TABLE "clientInfo" (
-	`rfidNumber`	TEXT,
-	`surname`	TEXT,
-	`name`	TEXT,
-	`gender`	TEXT,
-	`cellphoneNumber`	TEXT,
-	`adresNumber`	INT,
-	`zipcode`	TEXT,
-	`subscriptionName`	TEXT,
-	PRIMARY KEY(rfidNumber)
-);
-INSERT INTO `clientInfo` VALUES ('[52,188,189,222,235]','Branderhorst','Rohit','male','06-61927142',130,'7891DM','easy');
-INSERT INTO `clientInfo` VALUES ('[12,235,179,213,129]','van Rijs','Katrien','female','06-76193384',90,'1764HT','smart');
-CREATE TABLE "adresInfo" (
-	`zipcode`	TEXT,
-	`adresNumber`	INTEGER,
-	`street`	TEXT,
-	`town`	TEXT,
-	`country`	TEXT,
-	PRIMARY KEY(zipcode,adresNumber),
-	FOREIGN KEY(`zipcode`) REFERENCES clientInfo ( zipcode ),
-	FOREIGN KEY(`adresNumber`) REFERENCES clientInfo ( adresNumber )
-);
-INSERT INTO `adresInfo` VALUES ('7891DM',130,'Melde','Klazienaveen','Nederland');
-INSERT INTO `adresInfo` VALUES ('1764HT',90,'Irissenstraat','Breezand','Nederland');
-COMMIT;
