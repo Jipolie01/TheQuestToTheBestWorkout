7bee70df19a3690120f71a52b30b89a85e88148f
Maarten <maarten.deklerk@gmail.com>
2016-01-20 14:21:10 +0100

    added option access data from mysql database

--- src/accessMySQL.py
+++ src/accessMySQL.py
@@ -1,17 +1,43 @@
 import mysql.connector as sql
 from mysql.connector import errorcode
+import getpass
 
-try:
-    cnx = sql.connect(user='maarten', password='maartenmn',
-                      host = 'bennos-sportschool.adeklerk.nl',
-                      database = 'maarten')
+def connectDatabase():
+    p = getpass.getpass("Enter password: ")
 
-except sql.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+    try:
+        global cnx
+        cnx = sql.connect(user='admin', password=p,
+                          host = 'bennos-sportschool.adeklerk.nl',
+                          database = 'customer_db')
+
+    except sql.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
     else:
-        print(err)
-else:
+        print("connected to database")
+
+
+def getData():
+    connectDatabase()
+    cursor = cnx.cursor(buffered=True)
+
+    query = (
+        "SELECT RFIDNumber, surname, name FROM customerInfo"
+    )
+
+    cursor.execute(query)
+
+    for RFIDNumber, surname, name in cursor:
+        print("{} {} has RFID number {}".format(name, surname, RFIDNumber))
+
+    disconnectDatabase()
+
+def disconnectDatabase():
     cnx.close()
+
+getData()
--- src/sql/clientDatabase.sql
+++ src/sql/clientDatabase.sql
@@ -1,11 +1,69 @@
-CREATE TABLE IF NOT EXISTS costumerInfo (
-	costumerID		 INT UNSIGNED		NOT NULL AUTO_INCREMENT,
-    surname			 VARCHAR(30)		NOT NULL DEFAULT '',
-    name 			 VARCHAR(30)		NOT NULL DEFAULT '',
-    gender			 VARCHAR(6)			NOT NULL DEFAULT '',
-    RFIDNumber		 CHAR(20)			NOT NULL DEFAULT '[00,000,000,000,000]',
-    cellphoneNumber  CHAR(11)			NOT NULL DEFAULT '06-00000000',
-    subscriptionName VARCHAR(5)			NOT NULL DEFAULT '',
+create table if not exists costumerInfo (
+	costumerID		 int unsigned		not null auto_increment,
+    surname			 varchar(30)		not null default '',
+    name 			 varchar(30)		not null default '',
+    gender			 varchar(6)			not null default '',
+    RFIDNumber		 char(20)			not null default '[00,000,000,000,000]',
+    cellphoneNumber  char(11)			not null default '06-00000000',
+    subscriptionName varchar(5)			not null default '',
     
     primary key(costumerID)
 );
+
+insert into costumerInfo (costumerID, surname, name, gender, RFIDNumber, cellphoneNumber, subscriptionName)
+values (1, 'Plugge', 'Riemer', 'male', '[52,188,222,235]', '06-80971817', 'flex'); 
+
+
+create table if not exists adresInfo (
+	costumerID		int unsigned	not null auto_increment,
+    street			varchar(30)		not null default '',
+    zipcode			char(6)			not null default '0000AA',
+    adresNumber		varchar(5)		not null default '',
+    town			varchar(50)		not null default '',
+    country			varchar(20)		not null default 'Nederland',
+	
+    primary key(costumerID),
+    foreign key(costumerID) references costumerInfo(costumerID)
+);
+
+insert into adresInfo (costumerID, street, zipcode, adresNumber, town, counrty)
+values (1, 'De Beers', '5373CZ', '17', 'Herpen', 'Nederland');
+
+create table if not exists loginInfo (
+	costumerID		int unsigned	not null auto_increment,
+    username		varchar(12)		not null default '',
+    password		varchar(30)		not null default '',
+    email			varchar(254)	not null default '',
+    
+	primary key(costumerID),
+    foreign key(costumerID) references costumerInfo(costumerID)
+);
+
+insert into loginInfo (costumerID, username, password, email)
+values (1, 'Thasce', 'utai6eoquaT', 'RiemerPlugge@teleworm.us');
+
+create table if not exists products (
+	subscriptionName VARCHAR(5)			not null default '',
+    price			 int(5)				not null default '',
+    
+    primary key(subscriptionName),
+    foreign key(subscriptionName) references costumerInfo(subscriptionName)
+);
+
+insert into products (subscriptionName , price)
+values ('easy', 17.99);
+insert into products (subscriptionName , price)
+values ('smart', 19.99);
+insert into products (subscriptionName , price)
+values ('flex', 23.99);
+
+create table if not exists customerPerformanceInfo(
+	customerID		int unsigned	not null auto_increment,
+    startSession	datetime		not null default '0000-00-00 00:00:00',
+    endSession		datetime		not null default '0000-00-00 00:00:00',
+    fitnessDevice	varchar(13)		not null default '',
+    BurntCalories	smallint(5) 	not null default 0,
+    
+    primary key(startSession),
+    foreign key(costumerID) references costumerInfo(costumerID)
+); 
